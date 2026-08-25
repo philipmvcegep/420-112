9038a61121f984fcee8aa57db8238ff546530bcd
philipmvcegep <philip.higgins@cegepmv.ca>
2026-08-25 13:17:29 -0400

    first commit

---
+++ public/searchindex.en.js
@@ -0,0 +1,18 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "420-C2A Contenu web",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "420-C2A Contenu web",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
